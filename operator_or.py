0d24654b79e7b8af1d3f8e3ca025086259d9fffa
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-01-16 19:27:45 +0700

    ulta

---
+++ operator_or.py
@@ -0,0 +1,18 @@
+print("pada tanggal berapa merry lahir")
+
+print("A. 19 April 2003")
+print("B. 23 Juni 2002")
+print("C. 02 April 2003")
+print("D. 12 desember 2001")
+
+jawaban = str(input("jawaban = "))
+
+if (jawaban == "A" or jawaban == "a"):
+    print("ops,19 April tahun ini adalah ultanya merry berikan hadiah yah")
+elif (jawaban == "B" or jawaban == "b"):
+    print("ops,23 Juni tahun ini adalah hari bahagia merry berikan doa yah")
+elif (jawaban == "C" or jawaban == "c"):
+    print("ops,02 April tahun ini adalah hari happy sempro untuk merry")
+elif (jawaban == "D" or jawaban == "d"):
+    print("ops,12 Desember tahun ini adalah hari wisudanya merry beri selamat yah")
+    
